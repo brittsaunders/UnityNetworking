bad16e410708ec27089455f6e6af5bce5fee7efc
Brittany Saunders <32345478+brittsaunders@users.noreply.github.com>
2018-10-15 02:54:05 -0700

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Character : NetworkBehaviour
+{
+
+    public int count;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<Character>().enabled = true;
+            LookAtCamera.target = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().materials[0].color = Color.blue;
+
+    }
+
+}
---
+++ FlockA.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockA : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public static int size = 20;
+    public float speed = 5f;
+
+    static int amount = 150;
+    public static GameObject[] flock = new GameObject[amount];
+    public static Vector3 goTo = Vector3.zero;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-size, size), Random.Range(-size, size), Random.Range(-size, size));
+            flock[i] = (GameObject)Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goTo = prefab.transform.position - transform.position;
+        goTo = goTo.normalized;
+        transform.Translate(goTo * Time.deltaTime * speed, Space.World);
+    }
+}
